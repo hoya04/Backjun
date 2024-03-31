05e614154ee2e92e637cd150fd4846769266d6b3
hoya04 <164477482+hoya04@users.noreply.github.com>
2024-03-31 21:59:28 +0900

    [Bronze V] Title: N 찍기, Time: 1076 ms, Memory: 34092 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+
+        for (int n = 1; n <= i; n++) {
+            System.out.println(n);
+        }
+
+
+
+    }
+}
