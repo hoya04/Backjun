9e87f53ec128b607249401415c33b84a10eef3e9
JEONG HO SHIN <164477482+hoya04@users.noreply.github.com>
2024-08-20 12:40:50 +0900

    [Bronze V] Title: N 찍기, Time: 884 ms, Memory: 33664 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -2,14 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int i = sc.nextInt();
-
-        for (int n = 1; n <= i; n++) {
-            System.out.println(n);
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1;  i <= n; i++) {
+            System.out.println(i);
         }
-
-
-
     }
 }
